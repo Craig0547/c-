db3a70f218753de86b1481751f58e9b44c0288c7
Craig0547 <124737579+Craig0547@users.noreply.github.com>
2023-02-08 09:16:17 +0100

    Update punkt w trójkącie.cpp

--- punkt w trójkącie.cpp
+++ punkt w trójkącie.cpp
@@ -1,4 +1,4 @@
-#include<algorithm>
+
 #include<iostream>
 #include <cmath>
 using namespace std;
@@ -6,23 +6,7 @@ using namespace std;
 //funkcja obliczaj¹ca pole trójk¹ta o danych wspó³rzêdnych
 float pole(int x1, int y1, int x2, int y2, int x3, int y3)
 {
-int a, b, c, p;
-a=sqrt((x2-x1)^2+(y2-y1)^2);
-b=sqrt((x3-x2)^2+(y3-y2)^2);
-c=sqrt((x1-x3)^2+(y1-y3)^2);
-p=(a+b+c)/2;
-//wzór Herona 
-return sqrt(p*(p-a)*(p-b)*(p-c))
-}
-
-//funkcja sprawdzająca czy zmienne float są mniej więcej równe
-//credit to author
-bool approx_equal(double x, double y)
-{  
-   const double EPSILON = 1E-14;
-   if (x == 0) return fabs(y) <= EPSILON;
-   if (y == 0) return fabs(x) <= EPSILON;
-   return fabs(x - y) / max(fabs(x), fabs(y)) <= EPSILON;
+return abs((x1*(y2-y3) + x2*(y3-y1)+ x3*(y1-y2))/2.0);
 }
 
 //funkcja sprawdzaj¹ca czy pola BCD, ACD i ABD s¹ równe ABC
@@ -35,7 +19,7 @@ float P2 = pole(x1, y1, x, y, x3, y3);
 float P3 = pole(x1, y1, x2, y2, x, y);
 
 
-return approx_equal(P, P1+P2+P3);
+return (P == P1+P2+P3);
 }
 
 
